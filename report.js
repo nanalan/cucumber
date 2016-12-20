ad46af282fadd367767bd3115ca12dbf19f63368
Alex <16batesa@gmail.com>
2016-12-20 15:09:26 +0000

    Potentially fix windows `npm test`

--- report.js
+++ report.js
@@ -1,4 +1,13 @@
 const reporter = require('cucumber-html-reporter')
+const exec = require('child_process').execSync
+let failed = false
+
+try {
+  exec('node node_modules/cucumber/bin/cucumber.js -f json:report.json', { stdio: 'inherit' })
+} catch(e) {
+  failed = true
+  //console.error(e)
+}
 
 reporter.generate({
   theme: 'bootstrap',
@@ -8,3 +17,5 @@ reporter.generate({
   launchReport: false,
   name: 'Test Results',
 })
+
+process.exit(failed ? 1 : 0)
